c767eae0bef121a2bd0b11bffd6a8380e5d3c855
christopherlovell <chrisl1991@hotmail.co.uk>
2020-06-19 10:43:22 -0400

    change units

--- make_image_single_wavelength.py
+++ make_image_single_wavelength.py
@@ -6,6 +6,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 from hyperion.model import ModelOutput
+from astropy.cosmology import Planck15 as cosmo
 import astropy.units as u
 
 wav = 850  # micron
@@ -13,7 +14,15 @@ _dir = '/home/c.lovell/data/ufrc/simba/m25n512/run/snap_159/'
 ids = np.loadtxt('%s/ids.txt'%_dir,dtype=int)
 
 
+
+
+z = 2.025
+luminosity_distance = cosmo.luminosity_distance(z).to(u.cm)
+
 for _i,_id in enumerate(ids):
+
+    if _i != 14:
+        continue
 
     f = glob.glob('%s/gal_%i/snap159.galaxy*.rtout.image'%(_dir,_id))
     if len(f) < 1:
@@ -23,7 +32,8 @@ for _i,_id in enumerate(ids):
         m = ModelOutput(f[0])
     
         # Get the image from the ModelOutput object
-        image = m.get_image(units='ergs/s')
+        image = m.get_image(units='Jy', distance=luminosity_distance.value)
+        # image = m.get_image(units='ergs/s')#, distance=luminosity_distance.value)
         
         fig,ax = plt.subplots(1,1)
         
@@ -34,19 +44,24 @@ for _i,_id in enumerate(ids):
         w = image.x_max * u.cm
         w = w.to(u.kpc)
         
+        np.savetxt('arrays/gal_%i_850.txt'%_id, image.val[0,:,:,iwav])
+        print(image.val.shape)
+        print(image.val[0,:5,:5,iwav])
+
         # plot the beast
-        cax = ax.imshow(np.log(image.val[0, :, :, iwav]), cmap=plt.cm.viridis,
+        cax = ax.imshow(np.log10(image.val[0, :, :, iwav]), cmap=plt.cm.viridis,
                         origin='lower', extent=[-w.value, w.value, -w.value, w.value])
         
         
         # Finalize the plot
         ax.tick_params(axis='both', which='major', labelsize=10)
         ax.set_xlabel('x (kpc)')
-        ax.set_xlabel('y (kpc)')
+        ax.set_ylabel('y (kpc)')
         
-        plt.colorbar(cax, label='log Luminosity (ergs/s)', format='%.0e')
+        # plt.colorbar(cax, label='log Luminosity (ergs/s)', format='%.0e')
+        plt.colorbar(cax, label='$\mathrm{log_{10} \; Flux \; (Jy)}$', format='%.0e')
         
         # plt.show()
         fig.savefig('images/gal_%i_850.png'%_id, bbox_inches='tight', dpi=250)
         plt.close() 
-    
+ 
